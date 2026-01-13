6f8015057c67d59bb6d55d4ec7a83a68303284d9
arsumaehae <arsumaehae@gmail.com>
2026-01-13 10:38:25 +0700

    first step of FT for layout + styling

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -9,6 +9,31 @@ MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
+    def test_layout_and_styling(self):
+        # Edith ไปที่หน้าแรก
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # เธอสังเกตเห็นว่ากล่อง input จัดวางอยู่อย่างสวยงามตรงกลาง
+        inputbox = self.browser.find_element(By.ID, "id_new_item") # <--- หา element ก่อน
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # เธอเริ่ม list ใหม่ และเห็นว่า input ก็ยังอยู่ตรงกลางดี
+        inputbox.send_keys("testing")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: testing")
+        
+        inputbox = self.browser.find_element(By.ID, "id_new_item") # <--- หาใหม่หลัง reload
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
     def setUp(self):
         self.browser = webdriver.Chrome()
 
